19fa06e089368f2cd4ef6e7f52f6a39137f79c41
LenaDurant <durant.arlena@gmail.com>
2024-07-22 19:18:17 -0400

    finished tab 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import { useState } from 'react'
+import {puppyList} from './data.js'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-       
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList:", puppyList);
+  
+return (
+    
+      <div className="App"> 
+        {  
+           puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>;
+   })
+        }
       </div>
-      <h1></h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
