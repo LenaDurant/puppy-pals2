5759cba8dd0f7087e986a6b78dc92873c10037c4
LenaDurant <durant.arlena@gmail.com>
2024-07-22 20:43:55 -0400

    got the click to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { useState } from 'react'
-import {puppyList} from './data.js'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null)
-  
-  
-return (
-    
-      <div className="App"> 
-        {puppies.map((puppy) => {
-         {()=>{console.log("puppy id: ", puppy.id)}}   
-     return (
-     <p onClick={()=>{}} key={puppy.id}>{puppy.name}</p>
-     );
-   })
-        }
-      </div>
+
+  function handleClick() {
+    { () => { console.log("puppy id: ", puppy.id) } }
+  }
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick= { () => { console.log("puppy id: ", puppy.id) } } key={puppy.id}>{puppy.name}</p>
+        );
+      })
+      }
+    </div>
   );
 }
 
